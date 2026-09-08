540e172fc11d62f9c293948326affabd26e45a54
Ifeanyi Azikiwe <ifeanyi.azikiwe.jnr@gmail.com>
2026-09-09 00:55:36 +0100

    add scroll to top on page navigation

--- src/App.js
+++ src/App.js
@@ -201,7 +201,7 @@ export default function App() {
     <AuthProvider>
       <BrowserRouter>
         <ScrollToTop />
-        <AppRoutes />
+          <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
   );
